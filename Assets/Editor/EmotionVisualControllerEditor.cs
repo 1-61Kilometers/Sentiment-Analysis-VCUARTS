79082e8d879d13810c2ceeac9f6bffb34f42d0e2
hcbryant9 <bryanthc@vcu.edu>
2024-08-11 17:12:46 -0400

    Syncing naming system

--- Assets/Editor/EmotionVisualControllerEditor.cs
+++ Assets/Editor/EmotionVisualControllerEditor.cs
@@ -2,6 +2,7 @@ using UnityEditor;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+
 [CustomEditor(typeof(EmotionVisualController))]
 public class EmotionVisualControllerEditor : Editor
 {
@@ -9,44 +10,33 @@ public class EmotionVisualControllerEditor : Editor
     {
         DrawDefaultInspector();
 
-        if (GUILayout.Button("Add New Emotion to All Materials"))
+        if (GUILayout.Button("Rename 'Anger' to 'Angry'"))
         {
-            AddNewEmotionToAllMaterials();
+            RenameAngerToAngry();
         }
     }
 
-    private void AddNewEmotionToAllMaterials()
+    private void RenameAngerToAngry()
     {
         EmotionVisualController controller = (EmotionVisualController)target;
-
-        string newEmotion = "Sad"; // Name of the new emotion
-        Texture2D defaultTexture = null;  // Default texture for the new emotion
 
         // Access materialTextureSets via the getter
         List<EmotionVisualController.MaterialTextureSet> materialTextureSets = controller.GetMaterialTextureSets();
 
         foreach (var materialSet in materialTextureSets)
         {
-            // Check if the emotion already exists
-            if (materialSet.textures.Exists(texture => texture.emotion == newEmotion))
+            foreach (var textureData in materialSet.textures)
             {
-                Debug.LogWarning($"Emotion {newEmotion} already exists for material {materialSet.material.name}");
-                continue;
+                if (textureData.emotion == "Anger")
+                {
+                    textureData.emotion = "Angry";
+                    Debug.Log($"Renamed 'Anger' to 'Angry' for material {materialSet.material.name}");
+                }
             }
-
-            // Create a new TextureData for the new emotion
-            EmotionVisualController.TextureData newTextureData = new EmotionVisualController.TextureData
-            {
-                emotion = newEmotion,
-                texture = defaultTexture
-            };
-
-            // Add the new TextureData to the material set
-            materialSet.textures.Add(newTextureData);
         }
 
         // Mark the object as dirty to ensure changes are saved
         EditorUtility.SetDirty(controller);
-        Debug.Log("Added new emotion to all materials.");
+        Debug.Log("Completed renaming 'Anger' to 'Angry'.");
     }
 }
